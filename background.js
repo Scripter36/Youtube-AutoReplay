f9bd3321f02b5352109c1c29a493f8fa3668e51b
John Doe <dlrwnsaka@naver.com>
2017-08-17 19:36:01 +0900

    Iframe Support

--- background.js
+++ background.js
@@ -1,14 +1,11 @@
 var last;
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    window.tab = tab;
-    if (tab.status == "complete") {
-        //alert("loaded your page! hooray!");
-        chrome.tabs.executeScript(tab.id, {
-            file: "inject.js"
-        }, function () {
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError.message);
-            }
-        });
-    }
+    chrome.tabs.executeScript(tab.id, {
+        file: "inject.js",
+        allFrames: true
+    }, function () {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+        }
+    });
 });
